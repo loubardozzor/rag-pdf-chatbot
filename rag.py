f12046c0578149ac971a28565ed0f7ae9146218a
Hyacinthe <hyacinthetchoula@gmail.com>
2026-06-12 08:56:01 +0200

    configuration de cursor et ecriture echargement de la clé API openAI dans le code pour pouvoir acceder aux fonctionnalités proposé par openAI

--- rag.py
+++ rag.py
@@ -3,4 +3,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Chroma
 from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+open_ai_key = os.environ.get("OPEN_API_KEY")
