5ac6e997b54f671486e73c77ee4fe9f632019c45
Hyacinthe <hyacinthetchoula@gmail.com>
2026-06-15 17:38:33 +0200

    scritto la funzione per l'ingestione nella DB i chunks del documento e la funzione per il retrieval

--- rag.py
+++ rag.py
@@ -1,7 +1,8 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma, chroma
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 import os
@@ -10,10 +11,11 @@ import os
 load_dotenv()
 open_ai_key = os.environ.get("OPENAI_API_KEY")
 
-def text_processing(document):
+def text_processing(document:str)->list:
     """
         description: split a document into chunk
-        args: document a test document we want to split into chunks
+        args: 
+            document: a text document we want to split into chunks
         return: a list of chunks
     """
     loader = PyPDFLoader(document)
@@ -24,3 +26,24 @@ def text_processing(document):
 
 
 
+
+def ingest_pdf_to_DB(chunks:list)->Chroma:
+    """
+        description: this function compute the embedding of every chunk and ingest it into chroma
+        args:
+            chunks: list of documents chunk
+        return: the vector store
+    """ 
+    embedder =  OpenAIEmbeddings()
+    return Chroma.from_documents(chunks,embedder )
+    
+def get_relevant_docs(query: str, vector_store: Chroma)->list:
+    """
+        description: this function find the most relevant document for the query
+        args:
+            query: query write by the user about its documents
+            vector_store: the Chroma vector store instance
+
+        return: a list of documents relevant to the user's query
+    """
+    return vector_store.similarity_search(query)
