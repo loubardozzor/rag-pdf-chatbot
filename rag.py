fbd6c504061c7531653d75a524a72a7f0220cf85
Hyacinthe <hyacinthetchoula@gmail.com>
2026-06-15 16:49:32 +0200

    we have create function that devide a document into chunks

--- rag.py
+++ rag.py
@@ -6,5 +6,21 @@ from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()
-open_ai_key = os.environ.get("OPEN_API_KEY")
+open_ai_key = os.environ.get("OPENAI_API_KEY")
+
+def text_processing(document):
+    """
+        description: split a document into chunk
+        args: document a test document we want to split into chunks
+        return: a list of chunks
+    """
+    loader = PyPDFLoader(document)
+    pages = loader.load()
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = splitter.split_documents(pages)
+    return chunks
+
+
+
