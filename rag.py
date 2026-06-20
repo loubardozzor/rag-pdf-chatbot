e42163aa1a99036afb7bf34fcd693d4c40daed4b
Hyacinthe <hyacinthetchoula@gmail.com>
2026-06-20 15:04:32 +0200

    finito la parte logica dell'applicazione con la funzione che permette di contattare llm per formattare la risposta alla query dell'utente

--- rag.py
+++ rag.py
@@ -3,9 +3,11 @@ from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Chroma, chroma
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 from dotenv import load_dotenv
 import os
+
+from openai.types.responses import response
 
 
 load_dotenv()
@@ -36,6 +38,8 @@ def ingest_pdf_to_DB(chunks:list)->Chroma:
     """ 
     embedder =  OpenAIEmbeddings()
     return Chroma.from_documents(chunks,embedder )
+
+    
     
 def get_relevant_docs(query: str, vector_store: Chroma)->list:
     """
@@ -47,3 +51,30 @@ def get_relevant_docs(query: str, vector_store: Chroma)->list:
         return: a list of documents relevant to the user's query
     """
     return vector_store.similarity_search(query)
+
+
+
+
+def get_response_llm(query: str, chunks:list)->str:
+    """
+        description: this function get the response to the user query  from the LLM passing to it the context
+        args:
+            query: user query write by the user about its documents
+            chuncks: relevant documents to the user query getting from the vector db
+        return :
+                the answer formated as string from the user query      
+
+    """
+
+    context = " ".join([chunk.page_content for chunk in chunks]) # pythonic , more profesional
+    template = ChatPromptTemplate.from_messages([
+    ("system", "Sei un assistente utile che risponde usando solo il contesto fornito."),
+    ("user", "Contesto: {context}\nDomanda: {question}")])
+
+    llm = ChatOpenAI(model="gpt-4",
+                     temperature=0.2)
+    chain = template|llm
+    response = chain.invoke({"context":context, "question":query})
+    return response
+
+
