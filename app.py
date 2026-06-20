9537ea99743fd312c031e82031b2d6dea4e855cd
Hyacinthe <hyacinthetchoula@gmail.com>
2026-06-20 16:28:58 +0200

    ..

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ import streamlit as st
 st.title("Chat with Your Documents")
 uploaded_file = st.sidebar.file_uploader(label='carica il tuo PDF',
                         type =["pdf"])
+
+if uploaded_file is not None:
+    # procssare il file 
+    with open(uploaded_file.name, "wb") as f: # apriamo un file in modalta scrittura col nome del file caricato su streamlit e copiamo poi il sui contenuto nel file 
+        f.write(uploaded_file.getbuffer())
+    chunks = text_processing(uploaded_file.name)
+    chroma_db = ingest_pdf_to_DB(chunks)
+
